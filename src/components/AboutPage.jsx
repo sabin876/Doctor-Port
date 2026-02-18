dfa252ec1b1fa6be2a70f8ec696b7e7421752e95
sabin876 <sabinsabin876@gmail.com>
2026-02-18 19:11:18 +0545

    Integrate Sporting Injury section into About page after doctor introduction

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import About from './About';
+import SportingInjury from './SportingInjury';
 import FAQ from './FAQ';
 import Contact from './Contact';
 
@@ -7,6 +8,7 @@ const AboutPage = () => {
     return (
         <main>
             <About />
+            <SportingInjury />
             <FAQ />
             <Contact />
         </main>
