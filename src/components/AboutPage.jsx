65cbf101e53372a19bbc175176b15fe5d270e4e5
sabin876 <sabinsabin876@gmail.com>
2026-02-18 19:07:02 +0545

    Remove hero section from About Page and optimize layout padding

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Hero from './Hero';
 import About from './About';
 import FAQ from './FAQ';
 import Contact from './Contact';
@@ -7,7 +6,6 @@ import Contact from './Contact';
 const AboutPage = () => {
     return (
         <main>
-            <Hero />
             <About />
             <FAQ />
             <Contact />
