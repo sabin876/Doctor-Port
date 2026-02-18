a3e3768e9defaf0d51fb90800c4d8d7a00129db6
sabin876 <sabinsabin876@gmail.com>
2026-02-18 19:12:56 +0545

    Finalize simplified About page by removing Hero, FAQ, and Contact sections

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import About from './About';
 import SportingInjury from './SportingInjury';
-import FAQ from './FAQ';
-import Contact from './Contact';
 
 const AboutPage = () => {
     return (
         <main>
             <About />
             <SportingInjury />
-            <FAQ />
-            <Contact />
         </main>
     );
 };
